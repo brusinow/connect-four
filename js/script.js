8c48a9bb7ac27cbea65dcbc23a0c5d45ceecd6ba
brusinow <brusinow@me.com>
2016-04-04 01:02:41 -0700

    winning 4 pieces now flash, figured out array of winning coordinates

--- js/script.js
+++ js/script.js
@@ -11,9 +11,6 @@ var keepPlaying = true;
 
 var count = 0;
 
-
-
-
 var noMove = '<div class="circle-clear"></div>';
 var redMove = '<div class="circle-red"></div>';
 var blackMove = '<div class="circle-black"></div>';
@@ -22,14 +19,12 @@ var blackMove = '<div class="circle-black"></div>';
 
 $('#column-one-button').click(function(){
     var x = 0;
- 
   whichColumn(x);
   count++;
 });
 
 $('#column-two-button').click(function(){
     var x = 1;
- 
   whichColumn(x);
   count++;
 });
@@ -37,35 +32,30 @@ $('#column-two-button').click(function(){
 
 $('#column-three-button').click(function(){
     var x = 2;
-
   whichColumn(x);
   count++;
 });
 
 $('#column-four-button').click(function(){
     var x = 3;
-
   whichColumn(x);
   count++;
 });
 
 $('#column-five-button').click(function(){
     var x = 4;
- 
   whichColumn(x);
   count++;
 });
 
 $('#column-six-button').click(function(){
     var x = 5;
- 
   whichColumn(x);
   count++;
 });
 
 $('#column-seven-button').click(function(){
     var x = 6;
- 
   whichColumn(x);
   count++;
 });
@@ -75,74 +65,15 @@ function whoseTurn(count){
     if (count % 2 === 0){
     console.log('whoseTurn returns red move');
     return "R";
-  } else { 
+    } else { 
     console.log('whoseTurn returns black move'); 
     return "B"; 
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-// var makeMove = function(y, x, count){}
-//   var currentBox = board[count-1];
-//   var thisMoveOutcome = currentScore[i][j];
-//     if (0)
-
-
-
-//   currentBox.click(function(){
-//     if (currentBox.html === noMove && keepPlaying)
-//       currentBox.html = whoseTurn(count);
-//       currentScore.splice(i, 1, currentBox.innerText);
-
-
-
-
-  // });
-
-
-// for (var i = 0; i < board.length; i++){
-//   makeMove(i);
-// }
-
-
-// function running(win) {
-//   var win = getWinner(result);
-//   if (win) {
-//     keepPlaying = false;
-//     document.getElementById('whose-turn').textContent = 'We have a winner!';
-//     console.log('operation should stop');
-//   } else {
-//     console.log('keep playing...')
-//     keepPlaying = true;
-//   }
-// }
-
-
-
-// var getWinner = function(){}
-
-
-
-      
-
-
-
-
-
-
 function whichColumn(x){
-    // console.log("x is "+ x);
-    var y = 5;
+  var y = 5;
   if (keepPlaying){
     do {
       var dimensionChange = (7*y) + x;
@@ -151,25 +82,17 @@ function whichColumn(x){
         var redPlayer = "R";
         board[dimensionChange].className = 'eachCircle circle-red';
         currentScore[y].splice(x, 1, 'R');
-        console.log(currentScore);
-        console.log('red');
         getWinner(y,x,redPlayer);
         return true;
       }   else if (currentScore[y][x] === "" && whoseTurn(count)==="B"){
           var blackPlayer = "B";
           board[dimensionChange].className = 'eachCircle circle-black';
           currentScore[y].splice(x, 1, 'B');
-     
-          console.log(currentScore);
-          console.log('black');
           getWinner(y,x,blackPlayer);
           return false
           } else {
           y--;
-          console.log("something there");
-          console.log(y + " at bottom");
           }
-      // console.log("x is "+ x);
     }
   while(y >= 0);
   }
@@ -180,22 +103,28 @@ function whichColumn(x){
 function getWinner(lastPieceY, lastPieceX, player) { 
   var needFour = 1, 
      i = 1;
+  var winArray = [[lastPieceY,lastPieceX]];
+     console.log(lastPieceY, lastPieceX);
    // // across
-    while(currentScore[lastPieceY][lastPieceX - i] === player && (lastPieceX - i) >= 0 ) { 
+    while((lastPieceX - i) >= 0 && (currentScore[lastPieceY][lastPieceX - i]) === player ) {       
+      winArray.push([lastPieceY,(lastPieceX - i)]);
       needFour++; 
-      i++; 
+      i++;       
       console.log('horiz loop 1');
     };
       i = 1;
 
     while(currentScore[lastPieceY][lastPieceX + i] === player && (lastPieceX + i) <= 6) {
+      winArray.push([lastPieceY,(lastPieceX + i)]);
       needFour++; 
       i++; 
       console.log('horiz loop 2');
     };
 
       if (needFour >= 4) { 
+      console.log(winArray);
       console.log("win horizontal");
+      displayWinner(winArray);
       keepPlaying = false;
       return true; 
       }
@@ -203,8 +132,10 @@ function getWinner(lastPieceY, lastPieceX, player) {
 
     //down
       needFour = 1;
+     winArray = [[lastPieceY,lastPieceX]];
 
     while(((lastPieceY - i) >= 0)  && (currentScore[lastPieceY-i][lastPieceX] === player)) { 
+      winArray.push([(lastPieceY-i),lastPieceX]);
       needFour++; 
       i++; 
       console.log('vert loop 1');
@@ -212,6 +143,7 @@ function getWinner(lastPieceY, lastPieceX, player) {
       i = 1;
     
     while(((lastPieceY + i) <= 5) && (currentScore[lastPieceY+i][lastPieceX] === player)) {
+      winArray.push([(lastPieceY+i),lastPieceX]);
       needFour++; 
       i++; 
       console.log('vert loop 2');
@@ -219,6 +151,7 @@ function getWinner(lastPieceY, lastPieceX, player) {
 
       if (needFour >= 4) { 
       console.log("win vertical");
+      displayWinner(winArray);
       keepPlaying = false;
       return true; 
       }
@@ -226,8 +159,10 @@ function getWinner(lastPieceY, lastPieceX, player) {
 //------------------------------>
 // diagonal descending
     needFour = 1;
+      winArray = [[lastPieceY,lastPieceX]];
 
     while((lastPieceY - i) >= 0 && (lastPieceX - i) >= 0  &&  currentScore[lastPieceY-i][lastPieceX-i] === player ) { 
+      winArray.push([(lastPieceY-i),(lastPieceX-i)]);
       needFour++; 
       i++; 
       console.log('while loop 1');
@@ -236,6 +171,7 @@ function getWinner(lastPieceY, lastPieceX, player) {
     
 
     while((lastPieceY + i) <= 5 && (lastPieceX + i) <= 6  && currentScore[lastPieceY+i][lastPieceX+i] === player ) {
+      winArray.push([(lastPieceY+i),(lastPieceX+i)]);
       needFour++; 
       i++; 
       console.log('while loop 2');
@@ -243,14 +179,17 @@ function getWinner(lastPieceY, lastPieceX, player) {
 
       if (needFour >= 4) { 
       console.log("win diag descending");
+      displayWinner(winArray);
       keepPlaying = false;
       return true; 
       }
 
 // diagonal ascending
     needFour = 1;
+      winArray = [[lastPieceY,lastPieceX]];
 
     while((lastPieceY + i) <= 5 && (lastPieceX - i) >= 0  &&  currentScore[lastPieceY+i][lastPieceX-i] === player ) { 
+      winArray.push([(lastPieceY+i),(lastPieceX-i)]);
       needFour++; 
       i++; 
       console.log('while loop 1');
@@ -259,6 +198,7 @@ function getWinner(lastPieceY, lastPieceX, player) {
     
 
     while((lastPieceY - i) >= 0 && (lastPieceX + i) <= 6  && currentScore[lastPieceY-i][lastPieceX+i] === player ) {
+      winArray.push([(lastPieceY-i),(lastPieceX+i)]);
       needFour++; 
       i++; 
       console.log('while loop 2');
@@ -266,25 +206,24 @@ function getWinner(lastPieceY, lastPieceX, player) {
 
       if (needFour >= 4) { 
       console.log("win diag ascending");
+      displayWinner(winArray);
       keepPlaying = false;
       return true; 
       }
 
-
-
-
-
-
-
-
-
    console.log("no winner yet");
-   console.log("needFour is now "+needFour)
    return false;
 }
 
 
 
+function displayWinner(winArray){
+  for (i=0; i<winArray.length; i++){
+    var winSquare = (winArray[i][0] * 7)+winArray[i][1];
+    board[winSquare].className += " flash";
+    console.log("make an x");
+  }
+}
 
 
 
